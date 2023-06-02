ef1381fc05346e5d0b6bf22cbadcc3f41f1c7fff
Sc0lapasta <123011576+Sc0lapasta@users.noreply.github.com>
2023-06-02 22:50:04 +0200

    Add files via upload

---
+++ display.ino
@@ -0,0 +1,16 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(12,11,1,2,3,4); //l'ho messo al contrario per comodità
+//nota: mai toccare i cavi di alimentazione, sennò si bugga il display
+void setup()
+{
+  lcd.begin(16,2);
+}
+
+void loop()
+{
+  lcd.setCursor(0, 0);
+  lcd.print("02.06.2023");
+  lcd.setCursor(0, 1);
+  lcd.print("Luca gay");
+}
